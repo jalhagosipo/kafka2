90dc6ec31caaf2eff88d726494d720e6a6d5df25
jalhagosipo <gpfla757@naver.com>
2022-06-04 18:00:07 +0900

    feat: ProducerSync 추가
    - 동기 전송

---
+++ chapter3/src/main/java/ProducerSync.java
@@ -0,0 +1,32 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+import java.util.Properties;
+
+public class ProducerSync {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "kafka01.foo.bar:9092,kafka02.foo.bar:9092,kafka03.foo.bar:9092");
+        props.put("key.serializer",
+                "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer",
+                "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<>(props);
+
+        try {
+            for (int i = 0; i < 3; i++) {
+                ProducerRecord<String, String> record = new ProducerRecord<>("basic01", "Apache Kafka is a distributed streaming platform - " + i);
+                RecordMetadata metadata = producer.send(record).get();
+                System.out.printf("Topic: %s, Partition: %d, Offset: %d, Key: %s, Received Message: %s\n", metadata.topic(), metadata.partition()
+                        , metadata.offset(), record.key(), record.value());
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            producer.close();
+        }
+    }
+}
