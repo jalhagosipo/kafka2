98116eed241a93cfe4f40dece2a418a57ad630e5
jalhagosipo <gpfla757@naver.com>
2022-10-16 16:21:26 +0900

    feat: add chpater6 and python code

---
+++ chapter6/consumer_standard.py
@@ -0,0 +1,29 @@
+from confluent_kafka import Consumer
+
+broker = 'kafka01.foo.bar, kafka02.foo.bar, kafka03.foo.bar'
+group = 'consumer01'
+topic = 'test06'
+
+c = Consumer({
+    'bootstrap.servers': broker,
+    'group.id': group,
+    'auto.offset.reset': 'earliest'
+})
+c.subscribe([topic])
+
+while True:
+    msg = c.poll(1.0)
+
+    if msg is None:
+        continue
+    if msg.error():
+        print("Consumer error: {}".format(msg.error()))
+        continue
+    print('Topic: {}, '
+          'Partition: {}, '
+          'Offset: {}, '
+          'Received message: {}'.format(msg.topic(),
+                                        msg.partition(),
+                                        msg.offset(),
+                                        msg.value().decode('utf-8')))
+c.close()
